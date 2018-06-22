a83311b5093f6ba3ea91af54b06e33009b3b4254
V. Hong <khfy6hzy@nottingham.edu.my>
2018-06-22 11:41:18 +0800

    details component

---
+++ makan_gang/makan_gang/Components/RestaurantDetails.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {} from '@shoutem/ui'
+
+class RestaurantDetails extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+
+    )
+  }
+}
